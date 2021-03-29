1589bed295a9014b8d6a8ebbdd40bb85614247f2
Kyome22 <kyomesuke@gmail.com>
2021-03-30 01:15:10 +0900

    dependenciesをアップデートした。
    WriteFilePluginはwebpack5系とコンフリクトするので消した。

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const WriteFilePlugin = require("write-file-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -52,6 +51,5 @@ module.exports = {
         },
       ],
     }),
-    new WriteFilePlugin(),
   ],
 };
